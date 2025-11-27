ab407d08b9d08cba8465021a8f7ff221b282dd70
Sofiane KASBADJI <48035757+sofDji@users.noreply.github.com>
2025-11-27 17:53:05 +0100

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,7 +16,7 @@ export function Hero() {
       {/* Content */}
       <div className="container mx-auto max-w-6xl relative z-10">
         <div className="space-y-6 text-center md:text-left max-w-3xl">
-          <h1 className={`text-5xl md:text-7xl lg:text-8xl font-bold tracking-tight text-balance text-white drop-shadow-lg`}>
+          <h1 className={`text-5xl md:text-7xl lg:text-8xl tracking-tight text-balance text-white drop-shadow-lg`}>
             Maria Nadjem
           </h1>
           <p className="text-2xs md:text-2xs text-white/90 max-w-2xl text-balance drop-shadow-md">
